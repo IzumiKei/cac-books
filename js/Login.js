267247d4c2afe8db58b8a0bc8ae8e4976f7e8bca
Paau93 <141678206+Paau93@users.noreply.github.com>
2024-05-11 23:21:04 -0300

    Validación de datos
    Agregué el login, imagenes de fondo, valide los datos del registro y login y sus respectivos css

---
+++ js/Login.js
@@ -0,0 +1,29 @@
+const email = document.getElementById('email')
+const contra = document.getElementById('contra')
+const form = document.getElementById("form")
+const errores = document.getElementById("warning")
+const inicio = document.getElementById("inicio")
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let warning = "";
+    let error = false;
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    errores.innerHTML = "";
+    inicio.innerHTML = "";
+
+    if (!regexEmail.test(email.value)) {
+        warning += `Email inválido <br>`
+        error = true;
+    }
+    if (contra.value.lenght<8 || contra.value == "") {
+        warning += `Contraseña inválida <br>`
+        error = true;
+    }
+    if (error) {
+        errores.innerHTML = warning
+    } else {
+        inicio.innerHTML = "Sesión iniciada"
+    }
+})
+
---
+++ js/registrarse.js
@@ -0,0 +1,45 @@
+function registrarse() {
+    let nombre = document.getElementById('nombre');
+    let apellido = document.getElementById('apellido');
+    let email = document.getElementById('email');
+    let contra = document.getElementById('contra');
+    let contra2 = document.getElementById('contra2');
+    const form = document.getElementById("form");
+    const parrafo = document.getElementById("warning");
+    const enviado = document.getElementById("registrado");
+   
+    form.addEventListener("submit", e=>{
+        e.preventDefault();
+        let warning = "";
+        let error = false;
+        let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+        parrafo.innerHTML = "";
+        enviado.innerHTML = "";
+
+        if(nombre.value.length < 3){
+            warning += `Nombre inválido <br>`
+            error = true;
+        }
+        if(apellido.value.length < 2){
+            warning+=`Apellido inválido <br>`
+            error = true;
+        }
+        if(!regexEmail.test(email.value)){
+            warning += `Email inválido <br>`
+            error = true;
+        }
+        if(contra.value.length<8){
+            warning += `Contraseña inválida <br>`
+            error = true;
+        }
+        if(contra2.value !== contra.value){
+            warning += `La contraseña no coincide <br>`
+            error = true;
+        }
+        if(error){
+            parrafo.innerHTML = warning
+        }else{
+            enviado.innerHTML = "Formulario enviado"
+        }
+    })
+}
